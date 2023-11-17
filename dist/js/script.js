650805a85c9eaa609250a353edd3aa9167be8625
timgluk <timi.biz@bk.ru>
2023-11-17 14:27:09 +0300

    fix: article, accordion-js

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,15 @@ const buttonAccordion = [...document.querySelectorAll('.accordion-title-box')]
 
 buttonAccordion.map(el => {
   el.addEventListener('click', () => {
-    let text = el.nextElementSibling
+    const text = el.nextElementSibling
+    const arrow = el.querySelector('.accordion-arrow')
+
     if (text.classList.contains('accordion-text-visible')) {
       text.classList.remove('accordion-text-visible')
+      arrow.classList.remove('accordion-arrow_active')
     } else {
       text.classList.add('accordion-text-visible')
+      arrow.classList.add('accordion-arrow_active')
     }
   })
 })
