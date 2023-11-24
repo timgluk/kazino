cfca9c18c0d75e63db736d5213fae3f70d318a7f
timgluk <timi.biz@bk.ru>
2023-11-24 22:15:39 +0300

    add: page-news.html; widget - popular-news

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,134 +1,134 @@
 $(document).ready(function () {
-	//------------ Menu----------
-	$('.toggle').on('click', function () {
-		$(this).toggleClass('active')
-		$('.navigation').toggleClass('active')
-	})
+  //------------ Menu----------
+  $(".toggle").on("click", function () {
+    $(this).toggleClass("active");
+    $(".navigation").toggleClass("active");
+  });
 
-	// ------------------Modal----------------------
+  // ------------------Modal----------------------
 
-	$('.close').on('click', function () {
-		$(this).parents().removeClass('active')
-	})
+  $(".close").on("click", function () {
+    $(this).parents().removeClass("active");
+  });
 
+  $(".to-state").on("click", function (event) {
+    event.preventDefault();
+    $(".state").removeClass("active");
+    var state = $(this).attr("data-state");
+    $(".state[data-state=" + state + "]").addClass("active");
+    $("article").jScrollPane();
+  });
 
-	$('.to-state').on('click', function (event) {
-		event.preventDefault()
-		$('.state').removeClass('active')
-		var state = $(this).attr('data-state')
-		$('.state[data-state=' + state + ']').addClass('active')
-		$('article').jScrollPane()
-	})
+  jQuery(function ($) {
+    $(document).mouseup(function (e) {
+      // событие клика по веб-документу
+      var div = $(".state-box"); // тут указываем ID элемента
+      if (
+        !div.is(e.target) && // если клик был не по нашему блоку
+        div.has(e.target).length === 0
+      ) {
+        // и не по его дочерним элементам
+        div.parents().removeClass("active"); // скрываем его
+        div.removeClass("active"); // скрываем его
+      }
+    });
+  });
 
-	jQuery(function ($) {
-		$(document).mouseup(function (e) {
-			// событие клика по веб-документу
-			var div = $('.state-box') // тут указываем ID элемента
-			if (
-				!div.is(e.target) && // если клик был не по нашему блоку
-				div.has(e.target).length === 0
-			) {
-				// и не по его дочерним элементам
-				div.parents().removeClass('active') // скрываем его
-				div.removeClass('active') // скрываем его
-			}
-		})
-	})
+  // -------------------- Scroll -------------------------
 
-	// -------------------- Carousel--------------------------
+  $(".srolls").on("click", function (event) {
+    // исключаем стандартную реакцию браузера
+    event.preventDefault();
+    $(this).parents().removeClass("active");
+    $(".toggle").removeClass("active");
+    $("header").removeClass("active");
 
-	$('.cat-carousel-1').owlCarousel({
-		loop: false,
-		nav: true,
-		navText: ['', ''],
-		dots: false,
-		autoplay: false,
-		autoplayTimeout: 3000,
-		responsiveClass: true,
-		responsive: {
-			0: {
-				items: 1,
-				nav: false
-			},
-			600: {
-				items: 2
-			},
-			1000: {
-				items: 3
-			}
-		}
-	});
+    // получем идентификатор блока из атрибута href
+    var id = $(this).attr("href"),
+      // находим высоту, на которой расположен блок
+      top = $(id).offset().top - 50;
 
-	$('.cat-carousel-2').owlCarousel({
-		loop: false,
-		nav: true,
-		navText: ['', ''],
-		dots: false,
-		autoplay: false,
-		autoplayTimeout: 3000,
-		responsiveClass: true,
-		responsive: {
-			0: {
-				items: 1,
-				nav: false
-			},
-			600: {
-				items: 2
-			},
-			1000: {
-				items: 4
-			}
-		}
-	});
-	// -------------------- Scroll -------------------------
+    // анимируем переход к блоку, время: 800 мс
+    $("body,html").animate({ scrollTop: top }, 800);
+  });
 
-	$('.srolls').on('click', function (event) {
-		// исключаем стандартную реакцию браузера
-		event.preventDefault()
-		$(this).parents().removeClass('active')
-		$('.toggle').removeClass('active')
-		$('header').removeClass('active')
+  // -------------------- преобразуем svg картинку во встроеный svg--------------------------
+  function svg() {
+    $('img[src$=".svg"]').each(function () {
+      var $img = $(this);
+      var imgURL = $img.attr("src");
+      var attributes = $img.prop("attributes");
+      if ($img.hasClass("svg")) {
+        $.get(
+          imgURL,
+          function (data) {
+            var $svg = jQuery(data).find("svg");
+            $svg = $svg.removeAttr("xmlns:a");
+            $.each(attributes, function () {
+              $svg.attr(this.name, this.value);
+            });
+            $img.removeClass("svg").replaceWith($svg);
+          },
+          "xml"
+        );
+      }
+    });
+  }
+  svg();
 
-		// получем идентификатор блока из атрибута href
-		var id = $(this).attr('href'),
-			// находим высоту, на которой расположен блок
-			top = $(id).offset().top - 50
+  //--------------- fancybox -----------
+  $("[data-fancybox-popup]").fancybox({
+    closeExisting: true,
+    smallBtn: false,
+    toolbar: false,
+    autoFocus: false,
+    hash: false,
+    touch: false,
+  });
 
-		// анимируем переход к блоку, время: 800 мс
-		$('body,html').animate({ scrollTop: top }, 800)
-	})
+  // -------------------- Tabs -------------------------
 
-	
-	// -------------------- преобразуем svg картинку во встроеный svg--------------------------
-	function svg() {
-		$('img[src$=".svg"]').each(function () {
-			var $img = $(this);
-			var imgURL = $img.attr('src');
-			var attributes = $img.prop('attributes');
-			if ($img.hasClass('svg')) {
-				$.get(imgURL, function (data) {
-					var $svg = jQuery(data).find('svg');
-					$svg = $svg.removeAttr('xmlns:a');
-					$.each(attributes, function () {
-						$svg.attr(this.name, this.value);
+  (function ($) {
+    jQuery.fn.lightTabs = function (options) {
+      var createTabs = function () {
+        tabs = this;
+        i = 0;
+
+        showPage = function (i) {
+          $(tabs).children("div").children("div").hide();
+          $(tabs).children("div").children("div").eq(i).show();
+          $(tabs).find("ul.tabs-list").children("li").removeClass("active");
+          $(tabs).find("ul.tabs-list").children("li").eq(i).addClass("active");
+        };
+
+        showPage(0);
+
+        $(tabs)
+          .find("ul.tabs-list")
+          .children("li")
+          .each(function (index, element) {
+            $(element).attr("data-page", i);
+            i++;
+          });
+
+        $(tabs)
+          .find("ul.tabs-list")
+          .children("li")
+          .click(function () {
+            showPage(parseInt($(this).attr("data-page")));
+            /*if ($(this).data('page') != '3') {
+					$.each(players, function (n) {
+						for (let j = 0; j < players.length; j++) {
+							players[j].pause();
+						}
 					});
-					$img.removeClass('svg').replaceWith($svg);
-				}, 'xml');
-			}
-		});
-	}
-	svg();
+				}*/
+          });
+      };
+      return this.each(createTabs);
+    };
+  })(jQuery);
 
-	//--------------- fancybox -----------
-	$('[data-fancybox-popup]').fancybox({
-		closeExisting: true,
-		smallBtn: false,
-		toolbar: false,
-		autoFocus: false,
-		hash: false,
-		touch: false
-	});
-
-
-	//end
-})
+  $(".tabs").lightTabs();
+  //end
+});
